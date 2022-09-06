e2e7101c8b07a4e0abee457aac46027a4b9f98fa
Samantha <51409073+Tari38@users.noreply.github.com>
2022-09-06 16:37:19 +0100

    Delete src directory

--- src/assets/projects/index.js
+++
@@ -1,3 +0,0 @@
-export { default as StoryMashups } from "./story-mashups.png";
-export { default as HabiTrax } from "./habi-trax.png";
-export { default as RiskyQuizness } from "./risky-quizness.png";
